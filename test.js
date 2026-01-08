a68d0c3ae5f70f7703546d8a576915b793822d9c
Quang Vu <vudev205@gmail.com>
2026-01-08 16:25:50 +0700

    thêm trường time, vehicle, price, newPrice vào /tours/list

--- test.js
+++ test.js
@@ -254,7 +254,7 @@ app.get("/api/tours/list", async (req, res) => {
     const tours = await Tour.find({})
       .skip(start)
       .limit(limit)
-      .select("_id name category thumpnail_url")
+      .select("_id name category thumpnail_url time vehicle price newPrice")
       .lean()
       .maxTimeMS(5000);
 
